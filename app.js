84ba2853ade54f33e428be9e54847a77d9870131
oriionn <contact@oriondev.fr>
2024-02-28 00:58:49 +0100

    Ajout de Discord

--- app.js
+++ app.js
@@ -1,12 +1,37 @@
 const fastify = require('fastify')({
 	logger: process.argv[2] === '--dev'
 })
+require('dotenv').config();
 
-fastify.get("/discord", (req, res) => {
-	console.log(req.query);
+const Discord = require("discord.js");
+const client = new Discord.Client({ partials: Object.values(Object.fromEntries(new Map(Object.entries(Discord.Partials).filter((e) => Number.isInteger(e[1]))))),  intents: Object.values(Object.fromEntries(new Map(Object.entries(Discord.GatewayIntentBits).filter((e) => Number.isInteger(e[1]))))) });
+
+
+fastify.get("/discord", async (req, res) => {
+	let id = req.query.i;
+	if (!id) return res.send({ error: "No id provided" });
+	let user;
+	if (client.users.cache.has(id)) user = client.users.cache.get(id);
+	else user = await client.users.fetch(id);
+	if (!user) return res.send({ error: "User not found" });
+
+	let guild = client.guilds.cache.get(process.env.DISCORD_GUILD);
+	if (!guild) return res.send({ error: "Guild not found" });
+	let member = guild.members.cache.get(user.id);
+	if (!member) return res.send({ error: "Member not found" });
+
+	let username = user.username;
+	let avatar = user.avatarURL({ dynamic: true, size: 4096 });
+
+	let status;
+	if (!member.presence.activities[0]) status = "";
+	else status = member.presence.activities[0].state;
+
+	res.send({ username, avatar, status });
 })
 
-fastify.listen({ port: 3000, host: "0.0.0.0" }, (err, address) => {
+fastify.listen({ port: process.env.PORT || 3000, host: "0.0.0.0" }, (err, address) => {
 	if (err) throw err
 	console.log(`Server listening on ${address}`);
+	client.login(process.env.DISCORD_TOKEN);
 })
